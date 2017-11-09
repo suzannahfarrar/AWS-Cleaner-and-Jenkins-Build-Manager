ae23f7ac0a176b0c6b0425f1286252a81a4bade8
Suzannah <suzannahfarrar@gmail.com>
2017-11-10 01:21:33 +0530

    Add files via upload

--- ami_cleaner.py
+++ ami_cleaner.py
@@ -195,8 +195,9 @@ def main():
     env_name = args.env_name
 
     # Getting config information
-    ec = boto3.client('ec2', region_name='ap-south-1')
-    ec2 = boto3.resource('ec2', region_name='ap-south-1')
+    region = ['us-east-2','us-east-1','us-west-1','us-west-2','ca-central-1','ap-south-1','ap-northeast-2','ap-southeast-1','ap-southeast-2','ap-northeast-1','eu-central-1','eu-west-1','eu-west-2','sa-east-1']
+    ec = boto3.client('ec2', region_name=region)
+    ec2 = boto3.resource('ec2', region_name=region)
     ownerID = boto3.client('sts').get_caller_identity()['Account']
     
     get_ami(ec, ec2, no_of_ami, env_name, ownerID)
